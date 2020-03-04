8299907a81cfa6714ddf9315bb75ca2b92cea75a
Renildo Sousa <renildosousa75@gmail.com>
2020-03-03 21:10:36 -0300

    Estrutura inicial

---
+++ src/main/java/com/unitins/projetointegrador2/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.unitins.projetointegrador2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.unitins.projetointegrador2.model.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+}
