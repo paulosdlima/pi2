a6b5028df55be01f07588ad7154ea8b12a5240ca
Hygor de Sousa <hygor-araujo@hotmail.com>
2020-03-09 01:59:06 -0300

    Add framework de front-end

---
+++ src/main/java/com/unitins/projetointegrador2/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.unitins.projetointegrador2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("index")
+    public String index() {
+        return "index";
+    }
+}
